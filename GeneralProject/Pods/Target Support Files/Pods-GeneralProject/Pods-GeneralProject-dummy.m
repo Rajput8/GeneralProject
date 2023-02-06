e81b25f04b56933be95aeb35f0132fe3fbb6c863
Techwinlabs LLP <nidhi.techwinlabs@gmail.com>
2023-02-06 11:25:19 +0530

    pod deintegrate due to large size of project

--- GeneralProject/Pods/Target Support Files/Pods-GeneralProject/Pods-GeneralProject-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_GeneralProject : NSObject
-@end
-@implementation PodsDummy_Pods_GeneralProject
-@end
--- GeneralProject/Pods/Target Support Files/Pods-GeneralProjectTests/Pods-GeneralProjectTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_GeneralProjectTests : NSObject
-@end
-@implementation PodsDummy_Pods_GeneralProjectTests
-@end
