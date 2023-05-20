02da4551a6faef5771f50b71d576f1855734311f
Techwinlabs LLP <techwinlabs@gmail.com>
2023-05-20 16:55:46 +0530

    Integrate Loader Util
    Integrate Loader Util

--- GeneralProject/GeneralProject/Utilities/URLSessionManager/DataTask.swift
+++ GeneralProject/GeneralProject/Utilities/URLSessionManager/DataTask.swift
@@ -5,12 +5,12 @@ final class SessionDataTask {
     static func dataTask<T: Decodable>(type: T.Type, _ requestParams: APIRequestParams, _ completion: @escaping APIConstants.Handler<T>) {
 
         guard let request = SessionURLRequest.urlRequest(requestParams) else {
-            SwiftSpinner.hide()
+            LoaderUtil.hideLoading()
             completion(.failure(.invalidRequest))
             return
         }
-
-        // SwiftSpinner.show("Please wait", animated: true)
+        
+        LoaderUtil.showLoading()
         LogHandler.requestLog(request)
         Monitor().startMonitoring { [ ] connection, reachable in
             let reachableStatus = Monitor.getCurrentConnectivityStatus(connection, reachable)
@@ -18,11 +18,11 @@ final class SessionDataTask {
                 let task = URLSession.shared.dataTask(with: request, completionHandler: { (data, response, error) in
                     LogHandler.responseLog(data, response, error)
                     APIResponse.responseHandler(data, response, error, completion)
-                    SwiftSpinner.hide()
+                    LoaderUtil.hideLoading()
                 })
                 task.resume()
             } else {
-                SwiftSpinner.hide()
+                LoaderUtil.hideLoading()
                 APIRequestResources.remoteErrorAlert("no_internet_connection".localized(),
                                                      "device_connected_with_internet_warning".localized())
             }
