bd060b0c27c2d9b3199b8a6802d5c03c2027e08f
Techwinlabs LLP <iphone201988@gmail.com>
2023-08-18 19:24:43 +0530

    update uiview extension

--- GeneralProject/GeneralProject/Utilities/Extensions/ViewExt.swift
+++ GeneralProject/GeneralProject/Utilities/Extensions/ViewExt.swift
@@ -5,6 +5,27 @@ class CustomView: UIView {}
 
 extension UIView {
 
+    private struct AssociatedKey {
+        static var rounded = "UIView.rounded"
+    }
+
+    @IBInspectable var rounded: Bool {
+        get {
+            if let rounded = objc_getAssociatedObject(self, &AssociatedKey.rounded) as? Bool {
+                return rounded
+            } else {
+                return false
+            }
+        }
+        set {
+            DispatchQueue.main.async {
+                objc_setAssociatedObject(self, &AssociatedKey.rounded, newValue, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+                self.layer.cornerRadius = CGFloat(newValue ? 1.0 : 0.0)*min(self.bounds.width,
+                                                                            self.bounds.height)/2
+            }
+        }
+    }
+
     @IBInspectable var borderColor: UIColor? {
         get {
             let color = UIColor.init(cgColor: layer.borderColor!)
